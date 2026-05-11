2fc65eab9f4e3080418f636a884ab3620c159ed3
alvaronp401@gmail.com <silvio.filho_ext@light.com.br>
2026-05-11 12:44:44 -0300

    fix: cleanup payment preference structure and enable all methods

--- server/routes/paymentRoutes.js
+++ server/routes/paymentRoutes.js
@@ -101,6 +101,10 @@ router.post('/create-preference',
       }],
       payer: { email, name: fullName },
       external_reference: registrationId,
+      payment_methods: {
+        installments: 12, // Permite parcelamento em até 12x no cartão
+        default_payment_method_id: null
+      },
       back_urls: {
         success: `${frontendUrl}/success`,
         failure: `${frontendUrl}/checkout`,
