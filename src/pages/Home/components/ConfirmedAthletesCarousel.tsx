581d6923024354c64b52c17977c6a9b9f1de34cf
alvaronp401@gmail.com <silvio.filho_ext@light.com.br>
2026-05-15 16:58:25 -0300

    style: ajusta velocidade do carrossel para 19s

--- src/pages/Home/components/ConfirmedAthletesCarousel.tsx
+++ src/pages/Home/components/ConfirmedAthletesCarousel.tsx
@@ -51,8 +51,8 @@ export const ConfirmedAthletesCarousel: React.FC = () => {
           100% { transform: translateX(-25%); } /* Transladar 25% porque quadruplicamos o array */
         }
         .animate-marquee-horizontal {
-          /* Velocidade muito mais rápida, pedida pelo usuário: 12s */
-          animation: marquee-horizontal 12s linear infinite; 
+          /* Velocidade sincronizada com o MarqueeBanner e PartnersCarousel: 19s */
+          animation: marquee-horizontal 19s linear infinite; 
         }
         .pause:hover {
           animation-play-state: paused;
